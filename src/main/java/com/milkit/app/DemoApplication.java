e10951c56190635d7478512356eb34e3c35315b5
milkitmoon <milkit.moon@gmail.com>
2020-11-03 16:49:57 +0900

    DemoApplication.java 주석수정

--- src/main/java/com/milkit/app/DemoApplication.java
+++ src/main/java/com/milkit/app/DemoApplication.java
@@ -1,27 +1,14 @@
 package com.milkit.app;
 
 import java.sql.SQLException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.IntStream;
 
 import org.h2.tools.Server;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import com.milkit.app.domain.couponnoseq.service.CouponNoSeqServiceImpl;
 
 
 
@@ -37,7 +24,7 @@ public class DemoApplication extends SpringBootServletInitializer {
 		return biulder.sources(DemoApplication.class);
 	}
 	
-/*		외부 DB툴에서 메모리 DB접근을 원할 경우 주석해제
+/*		외부 DB툴에서 메모리 DB접근을 원할 경우 주석해제 (활성화 시 일부 Junit 테스트 실패할 수 있음)
 	@Bean(initMethod = "start", destroyMethod = "stop")
 	public Server h2Server() throws SQLException {
 	    return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9099");
